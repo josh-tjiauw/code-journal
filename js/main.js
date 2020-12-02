5729e489f1a5bc424efbb52853e542da28a54951
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-02 11:42:15 -0800

    Added names for the profile in javascript, and added the picture functionalityhttps://i.imgur.com/LkmPH6Y.png

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var
+var $avatarUrl = document.getElementById('avatarUrl');
+var $username = document.getElementById('username');
+var $fullName = document.getElementById('fullName');
+var $location = document.getElementById('location');
+var $bio = document.getElementById('bio');
+var $profilePicture = document.querySelector('img');
+
+$avatarUrl.addEventListener('input', function(event){
+  $profilePicture.src = event.target.value;
+})
