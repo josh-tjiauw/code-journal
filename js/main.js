b77e805678fd5854ba52c3f4ce66245051429d19
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-03 12:52:51 -0800

    Added the function that replicates the html for profile, also added the functionality where it takes the data from array, and puts it in the correct categories(eg. name, location, username, bio)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var $container = document.querySelector('.container');
 var $avatarUrl = document.getElementById('avatarUrl');
 var $username = document.getElementById('username');
 var $fullName = document.getElementById('fullName');
@@ -5,6 +6,7 @@ var $location = document.getElementById('location');
 var $bio = document.getElementById('bio');
 var $profilePicture = document.querySelector('img');
 var $form = document.querySelector('form');
+var $profile = document.getElementById('profile');
 
 $avatarUrl.addEventListener('input', function(event){
   $profilePicture.src = event.target.value;
@@ -15,8 +17,81 @@ $form.addEventListener('submit', function(event){
   data['profile']['username'] = $username.value;
   data['profile']['fullName'] = $fullName.value;
   data['profile']['location'] = $location.value;
-  data['profile']['avatarURL'] = $avatarUrl.value;
+  data['profile']['avatarUrl'] = $avatarUrl.value;
   data['profile']['bio'] = $bio.value;
   $form.reset();
   $profilePicture.src = 'images/placeholder-image-square.jpg';
 })
+
+function displayData(data){
+  //Full Name
+  var $rowOneDiv = document.createElement('div');
+  $rowOneDiv.className = 'row';
+  var $columnOneDiv = document.createElement('div');
+  $columnOneDiv.className = 'column-full';
+  var $h1FullName = document.createElement('h1');
+  $h1FullName.className = 'reg-text';
+  $h1FullName.id = 'displayFullName';
+  $h1FullName.textContent = data['profile']['fullName'];
+  //Profile Picture
+  var $rowTwoDiv = document.createElement('div');
+  $rowTwoDiv.className = 'row';
+  var $columnTwoDiv = document.createElement('div');
+  $columnTwoDiv.className = 'column-half';
+  var $displayProfilePicture = document.createElement('img')
+  $displayProfilePicture.alt = 'Profile Picture';
+  $displayProfilePicture.src = data['profile']['avatarUrl'];
+  //Username
+  var $columnTwoDiv2 = document.createElement('div');
+  $columnTwoDiv2.className = 'column-half';
+  var $h2displayUsername = document.createElement('h2');
+  $h2displayUsername.className = 'reg-text';
+  $h2displayUsername.innerHTML = '<i class="fas fa-user"></i>&Tab;'
+  var $spandisplayUsername = document.createElement('span');
+  $spandisplayUsername.id = 'displayUsername'
+  $spandisplayUsername.textContent = data['profile']['username'];
+  //Location
+  var $rowForLocation = document.createElement('div');
+  $rowForLocation.className = 'row';
+  var $h2displayLocation = document.createElement('h2');
+  $h2displayLocation.className = 'reg-text';
+  $h2displayLocation.innerHTML = '<i class="fas fa-map-marker-alt"></i>&Tab;'
+  var $spandisplayLocation = document.createElement('span');
+  $spandisplayLocation.id = 'displayLocation'
+  $spandisplayLocation.textContent = data['profile']['location'];
+  //Bio
+  var $rowForBio = document.createElement('div');
+  $rowForBio.className = 'row';
+  var $pdisplayBio = document.createElement('p');
+  $pdisplayBio.className = 'reg-text';
+  $pdisplayBio.id = 'displayBio'
+  $pdisplayBio.textContent = data['profile']['bio'];
+  //Append to doc
+  $rowForBio.appendChild($pdisplayBio);
+  $h2displayLocation.appendChild($spandisplayLocation);
+  $rowForLocation.appendChild($h2displayLocation);
+  $h2displayUsername.appendChild($spandisplayUsername);
+  $columnTwoDiv2.appendChild($h2displayUsername);
+  $columnTwoDiv2.appendChild($h2displayLocation);
+  $columnTwoDiv2.appendChild($pdisplayBio);
+  $columnTwoDiv.appendChild($displayProfilePicture);
+  $rowTwoDiv.appendChild($columnTwoDiv);
+  $rowTwoDiv.appendChild($columnTwoDiv2);
+  $columnOneDiv.appendChild($h1FullName);
+  $rowOneDiv.appendChild($columnOneDiv);
+  $profile.appendChild($rowOneDiv);
+  $profile.appendChild($rowTwoDiv);
+  return $profile;
+}
+displayData(data);
+$container.appendChild($profile);
+/*
+var $dataView = document.createElement('div');
+var att = document.createAttribute('data-view');
+att.value = 'profile';
+$dataView.setAttributeNode(att);
+var $displayFullName = document.getElementById('displayFullName');
+var $displayUsername = document.getElementById('displayUsername');
+var $displayLocation = document.getElementById('displayLocation');
+var $displayBio = document.getElementById('displayBio')
+var $displayProfilePicture = document.getElementById('displayProfilePicture');*/
